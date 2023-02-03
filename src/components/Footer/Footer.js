03505c90d7d3a8b7cfa66c8589066a1223bf6139
kush1012 <124149232+kush1012@users.noreply.github.com>
2023-02-03 15:49:27 +0530

    test

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,7 +9,7 @@ function Footer() {
         <div className="footer-content">
           <div className="footer-section about">
             <p className="footer-para">
-              Developer: <span className="my-name"> Naveen R</span>
+              Developer: <span className="my-name"> kushal</span>
             </p>
 
             <div className="contact"></div>
